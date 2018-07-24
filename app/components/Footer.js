534fc5753f637749d2fff26556ea61f2977470cc
linzisheng1995 <lin.zish@husky.neu.edu>
2018-07-24 14:39:19 -0700

    new

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -39,9 +39,8 @@ class Footer extends React.Component {
         <div className='container'>
           <div className='row'>
             <div className='col-sm-5'>
-              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
               <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
-              <p>You may view the <a href='https://github.com/sahat/newedenfaces-react'>Source Code</a> behind this project on GitHub.</p>
+              <p>You may view the <a href='https://github.com/ZishengLin/role-vote-system-react'>Source Code</a> behind this project on GitHub.</p>
               <p>© 2018 Zisheng Lin.</p>
             </div>
             <div className='col-sm-7 hidden-xs'>
